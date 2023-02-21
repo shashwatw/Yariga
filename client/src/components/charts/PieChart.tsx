95ce6ece6b1d41a309488b679a3986eb941e373c
shashwatwankhedekar <shashwatwankhedekar@gmail.com>
2023-02-21 12:01:49 +0530

    piechart added

--- client/src/components/charts/PieChart.tsx
+++ client/src/components/charts/PieChart.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import ReactApexChart from "react-apexcharts";
+import { Typography, Box, Stack } from "@pankod/refine-mui";
 
-const PieChart = () => {
-  return <div>PieChart</div>;
+import { PieChartProps } from "interfaces/home";
+
+const PieChart = ({ title, value, series, colors }: PieChartProps) => {
+  return (
+    <Box
+      id="chart"
+      flex={1}
+      display="flex"
+      bgcolor="#fcfcfc"
+      alignItems="center"
+      flexDirection="row"
+      justifyContent="space-between"
+      pl={3.5}
+      pr={2}
+      gap={2}
+      borderRadius={15}
+      minHeight="110px"
+      width="fit-content"
+    >
+      <Stack direction="column">
+        <Typography fontSize={14} color="#808191">
+          {title}
+        </Typography>
+        <Typography fontSize={24} color="#11142D" fontWeight={700} mt={1}>
+          {value}
+        </Typography>
+      </Stack>
+
+      <ReactApexChart
+        options={{
+          chart: { type: "donut" },
+          colors,
+          legend: { show: false },
+          dataLabels: { enabled: false },
+        }}
+        series={series}
+        type="donut"
+        width="120px"
+      />
+    </Box>
+  );
 };
 
 export default PieChart;
--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,5 +1,5 @@
 import { useList } from "@pankod/refine-core";
-
+import { Typography, Box, Stack } from "@pankod/refine-mui";
 import {
   PieChart,
   PropertyReferrals,
@@ -9,7 +9,40 @@ import {
 } from "components";
 
 const home = () => {
-  return <div>home</div>;
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Dashboard
+      </Typography>
+
+      <Box mt="20px" display="flex" flexWrap="wrap" gap={4}>
+        <PieChart
+          title="Properties for Sale"
+          value={664}
+          series={[75, 25]}
+          colors={["#275be8", "#c4e8ef"]}
+        />
+        <PieChart
+          title="Properties for Rent"
+          value={550}
+          series={[60, 40]}
+          colors={["#275be8", "#c4e8ef"]}
+        />
+        <PieChart
+          title="Total Customers"
+          value={5684}
+          series={[75, 25]}
+          colors={["#275be8", "#c4e8ef"]}
+        />
+        <PieChart
+          title="Properties for Cities"
+          value={550}
+          series={[75, 25]}
+          colors={["#275be8", "#c4e8ef"]}
+        />
+      </Box>
+    </Box>
+  );
 };
 
 export default home;
